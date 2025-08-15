db294cf5b23e107d1039994950b63bd0831e6cd1
Berl treasure Campomanes <berlcamp@gmail.com>
2025-08-15 23:28:47 +0800

    ..

--- components/CustomerOrdersModal.tsx
+++ components/CustomerOrdersModal.tsx
@@ -81,7 +81,7 @@ export const CustomerOrdersModal = ({
 
       {/* Centered panel container */}
       <div className="fixed inset-0 z-50 flex items-center justify-center p-4">
-        <DialogPanel transition className="app__modal_dialog_panel">
+        <DialogPanel transition className="app__modal_dialog_panel_lg">
           {/* Sticky Header */}
           <div className="app__modal_dialog_title_container">
             <DialogTitle as="h3" className="text-base font-medium flex-1">
@@ -108,7 +108,7 @@ export const CustomerOrdersModal = ({
                     <th className="app__th">Date</th>
                     <th className="app__th">SO Number</th>
                     <th className="app__th">Products</th>
-                    <th className="app__th">Total Amount</th>
+                    <th className="app__th text-right">Total Amount</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -120,27 +120,31 @@ export const CustomerOrdersModal = ({
                           ? format(new Date(item.created_at), 'MMMM dd, yyyy')
                           : 'Invalid date'}
                       </td>
+
                       <td className="app__td">
                         <span className="font-bold">{item.so_number}</span>
                       </td>
 
                       <td className="app__td">
-                        <span>
-                          {item.order_items.length > 0 &&
-                            item.order_items.map((item) => (
-                              <div key={item.id} className="space-x-2">
-                                <span>{item.product?.name}</span>
-                                <span>|</span>
-                                <span>
-                                  ({item.quantity} x <Php /> {item.unit_price})
-                                </span>
-                                <span></span>
-                              </div>
-                            ))}
-                          g
-                        </span>
+                        {item.order_items.length > 0 &&
+                          item.order_items.map((oi) => (
+                            <div
+                              key={oi.id}
+                              className="flex justify-between space-x-12 items-center"
+                            >
+                              <span className="">
+                                {oi.product?.name?.length > 70
+                                  ? oi.product.name.slice(0, 70) + '…'
+                                  : oi.product?.name}
+                              </span>
+                              <span className="whitespace-nowrap">
+                                ({oi.quantity} x <Php /> {oi.unit_price})
+                              </span>
+                            </div>
+                          ))}
                       </td>
-                      <td className="app__td">
+
+                      <td className="app__td text-right">
                         <span className="font-bold">
                           <Php />{' '}
                           {item.total_amount.toLocaleString(undefined, {
@@ -151,9 +155,10 @@ export const CustomerOrdersModal = ({
                       </td>
                     </tr>
                   ))}
+
                   {logs?.length === 0 && (
                     <tr className="app__tr">
-                      <td colSpan={3} className="app__td">
+                      <td colSpan={4} className="app__td text-center">
                         No change logs found
                       </td>
                     </tr>
